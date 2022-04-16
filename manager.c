886f5361faa387cc37a8eb696dee5d2426af4646
amazon8 <21900428@handong.ac.kr>
2022-04-16 17:00:27 +0900

    annotation

--- manager.c
+++ manager.c
@@ -12,7 +12,7 @@ bool printProduct(Product s[20]); // 함수에서 출력할 하나의 제품을 
 bool listProduct(Product s[20]); // 등록된 전체 제품 목록 출력
 bool updateProduct(Product s[20]); // 등록된 제품 제품을 수정
 bool deleteProduct(Product s[20]); // 제품 제품을 삭제
-bool saveFile(Product s[20]); // 파일에 저장
+bool saveFile(Product s[20]); // 등록된 제품 목록을 파일에 저장
 void loadFile(Product s[20]); // 파일을 로드해서 데이터를 p에 저장, 갯수는 전역변수 num에 반영
 void searchProduct(Product s[20]); // 함수에서 제품 이름을 묻고, 제품 이름을 검색
 void limitPrice(Product s[20]); // 함수에서 가격 두개를 입력 받고, 두 가격 사이의 제품들 검색
